062d44f2f6c3b9e919cdc691e85243f1aa7e252f
Konstantin Naumov <garfeeeld@mail.ru>
2021-08-22 22:36:14 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Menu } from "./components/Menu"
+
+export const App = () => {
+    return (
+        <div>
+            <Menu/>
+        </div>
+    )
+}
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from "react";
+
+export const Menu = (): ReactElement => {
+
+    const base = 'Menu'
+
+    return (
+        <div>{'HELLO!!!'}</div>
+    )
+}
